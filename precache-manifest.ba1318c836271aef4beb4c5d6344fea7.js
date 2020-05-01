24a3e10004c3bd5372f67b4eafde9b51d75436e6
Naldo Maranhão <reginaldo.maranhao@iteva.org.br>
2020-05-01 18:56:17 -0300

    first commit

---
+++ precache-manifest.ba1318c836271aef4beb4c5d6344fea7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3765d1ee7a0f2621b8dc3f834625e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aed1a2517fd1bd47092",
+    "url": "/static/css/2.cd31d209.chunk.css"
+  },
+  {
+    "revision": "4b96d8895152b57179e7",
+    "url": "/static/css/main.67816f9c.chunk.css"
+  },
+  {
+    "revision": "5aed1a2517fd1bd47092",
+    "url": "/static/js/2.99e9235d.chunk.js"
+  },
+  {
+    "revision": "4b96d8895152b57179e7",
+    "url": "/static/js/main.7da7df27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa80af172116c78d0e64583be2d4818f",
+    "url": "/static/media/logoFbuni.fa80af17.png"
+  },
+  {
+    "revision": "8c71f330ad67040b2e418bb60794d16b",
+    "url": "/static/media/logoSite.8c71f330.png"
+  }
+]);
